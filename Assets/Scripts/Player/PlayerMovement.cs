ee680ea31314c98212eaef32e89d716c29520908
mikepmerritt <mikepmerritt@gmail.com>
2024-05-22 12:58:14 -0400

    fix issue that caused player to slow down over time

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,24 +7,28 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private int index;
     [SerializeField] private float speed;
+    [SerializeField] private Vector2 direction;
+
+    private void FixedUpdate()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(direction.x * speed, GetComponent<Rigidbody2D>().velocity.y);
+    }
 
     public void Move(InputAction.CallbackContext context)
     {
         if(context.started)
         {
-            // Debug.Log("started");
+            Debug.Log("started");
         }
         else if(context.performed)
         {
-            // Debug.Log("performed");
-            Vector2 direction = context.ReadValue<Vector2>();
-            // GetComponent<Rigidbody2D>().velocity = direction * speed;
-            GetComponent<Rigidbody2D>().velocity = new Vector2(direction.x * speed, GetComponent<Rigidbody2D>().velocity.y);
+            Debug.Log("performed");
+            direction = context.ReadValue<Vector2>();
         }
         else if(context.canceled)
         {
-            // Debug.Log("canceled");
-            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            Debug.Log("canceled");
+            direction = Vector2.zero;
         }
     }
 
